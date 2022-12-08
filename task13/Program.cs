34772201bcb14d24e8954d64c54dc74f5294337c
Polin <polina996@mail.ru>
2022-12-08 20:35:07 +0300

    push

---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+if (N<100)
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
+else
+{
+    while (N>=1000)
+    {
+        N = N / 10;
+    }
+    int thirdNumber = N % 10;
+    Console.WriteLine($"Третья цифра --> {thirdNumber}");
+}
